3303a425ed95a7dac039388d52683b70be94a270
Ian Fan <ian.fan13@gmail.com>
2021-03-12 20:49:00 -0500

    challenge

--- static/js/ufo_starterCode.js
+++ static/js/ufo_starterCode.js
@@ -24,32 +24,28 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-let filterby={};
-
-//let filterby = {'datetime':"", 'city':"", 'state':"", 'country':"", 'shape':""};
+let filterBy={};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
     
     // 4a. Save the element that was changed as a variable.
-    var element = d3.select(this)
+    let element = d3.select(this)
     
     // 4b. Save the value that was changed as a variable.
-    var changedValue = element.property('value')
+    let changedValue = element.property('value')
 
     // 4c. Save the id of the filter that was changed as a variable.
-    var id = element.property('id')
+    let id = element.property('id')
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
 
     if (changedValue) {
-      filterby[id]=changedValue
-      console.log('yes')
+      filterBy[id]=changedValue
     }
     else {
-      delete filterby.id;
-      console.log('null')
+      delete filterBy.id;
     }
 
     // 6. Call function to apply all filters and rebuild the table
@@ -65,10 +61,15 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    /*insert for loop or foreach based off of filterby object*/
+    for (let filter in filterBy){
+      
+      if (filterBy[filter]) {
+        console.log('entered if of the loop properly')
+        console.log(row.filter)
+        filteredData = filteredData.filter(row => row.filter === filterBy[filter]);
+      };
+    }
     
-    //filteredData=filteredData.filter(row => row./**/=== filterby[/**/])
-  
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
